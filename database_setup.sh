88f6cf1a4457ae18d439bd8dd9a0599defec76e8
Alexis Cuison <30744307+acuison@users.noreply.github.com>
2024-02-13 08:28:11 +0800

    Create database_setup.sh

---
+++ database_setup.sh
@@ -0,0 +1,17 @@
+cd C:\xampp\mysql\bin
+mysqladmin -u root password your-root-password
+mysql.exe -u root -p
+CREATE USER 'Arduino'@'localhost' IDENTIFIED BY 'ArduinoGetStarted.com';
+GRANT ALL PRIVILEGES ON *.* TO 'Arduino'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+CREATE DATABASE db_arduino CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci';
+USE db_arduino;
+
+CREATE TABLE tbl_temp (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	date DATE,
+  time TIME,
+  med_name VARCHAR(60),
+  comp_num INT,
+	PRIMARY KEY (id)
+);
